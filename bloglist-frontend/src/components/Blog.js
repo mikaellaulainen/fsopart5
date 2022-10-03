10f84bca3677ef0ac92285ce8b03f300af9afde2
mikaellaulainen <laulainenmikael@gmail.com>
2022-10-03 12:52:03 +0300

    Done 5.7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,29 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [view,setView] = useState(false)
+
+  const handleClick= (event) => {
+    event.preventDefault()
+    setView(view ? false : true)
+  }
+
+  if(view){
+    return (
+    <div className="show">
+      <p>{blog.title}, {blog.author} <button onClick={handleClick}>Hide</button></p>
+       <p>Likes: {blog.likes} <button>Like</button></p> 
+       <p>Added by user: {blog.user.username}</p>
+    </div>
+    )
+  } else{
+    return (
+      <div className="show">
+        {blog.title}, {blog.author}
+        <button onClick={handleClick}>View</button>
+      </div>
+      )
+  }
+}
 
 export default Blog
